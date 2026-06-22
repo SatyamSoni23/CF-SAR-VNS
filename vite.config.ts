f1fbc5d0c25d8a3701658a503454b3b39b245964
Satyam Soni <42893127+SatyamSoni23@users.noreply.github.com>
2026-06-23 02:04:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/CF-SAR-VNS/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
